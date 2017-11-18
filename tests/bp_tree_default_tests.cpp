8aabf7891546fdd32f8a78f1f64e204f99216f20
joakimthun <joakimthun@gmail.com>
2017-11-18 07:49:53 +0100

    random_keys_test refactoring, re-added validate_bp_tree to random_keys_test

--- tests/bp_tree_default_tests.cpp
+++ tests/bp_tree_default_tests.cpp
@@ -22,37 +22,43 @@ void random_keys_test(std::size_t rand_seed, std::size_t num_keys)
         if (added_keys.find(key) != added_keys.end())
             continue;
 
-        auto i_res = t->insert(key, i);
-        EXPECT_EQ(true, i_res) << "insert key: " << key << ", index: " << i << ", seed:" << rand_seed;
-        auto result = validate_bp_tree(t);
-        EXPECT_EQ(true, result.valid) << result.message << std::endl << "insert key: " << key << ", index: " << i << ", seed:" << rand_seed;
-        i_res = t->insert(key, i);
-        EXPECT_EQ(false, i_res);
+        auto insert_result = t->insert(key, i);
+        EXPECT_EQ(true, insert_result) << "insert key: " << key << ", index: " << i << ", seed:" << rand_seed;
+
+        auto validate_result = validate_bp_tree(t);
+        EXPECT_EQ(true, validate_result.valid) << validate_result.message << std::endl << "insert key: " << key << ", index: " << i << ", seed:" << rand_seed;
+
+        insert_result = t->insert(key, i);
+
+        EXPECT_EQ(false, insert_result);
         added_keys.insert(key);
     }
 
-    auto i = 0;
+    auto added_keys_index = 0;
     auto keys_to_check = added_keys;
     for (const auto k : added_keys)
     {
-        auto rem = t->remove(k);
-        EXPECT_EQ(true, rem) << "remove key: " << k << " i: " << i << ", seed:" << rand_seed;
-        if (!rem)
+        auto remove_result = t->remove(k);
+        EXPECT_EQ(true, remove_result) << "remove key: " << k << " added_keys_index: " << added_keys_index << ", seed:" << rand_seed;
+        if (!remove_result)
             return;
 
+        auto validate_result = validate_bp_tree(t);
+        EXPECT_EQ(true, validate_result.valid) << validate_result.message << std::endl << "key: " << k;
+
         keys_to_check.erase(k);
-        auto ii = 0;
+        auto keys_to_check_index = 0;
         for (auto key : keys_to_check)
         {
-            auto exists = t->exists(key);
-            EXPECT_EQ(true, exists) << "exists key: " << key << " remove key: " << k << " i: " << i << ", seed:" << rand_seed << " ii: " << ii;
-            if (!exists)
+            auto exists_result = t->exists(key);
+            EXPECT_EQ(true, exists_result) << "exists key: " << key << " remove key: " << k << " added_keys_index: " << added_keys_index << ", seed:" << rand_seed << " keys_to_check_index: " << keys_to_check_index;
+            if (!exists_result)
                 return;
 
-            ii++;
+            keys_to_check_index++;
         }
 
-        i++;
+        added_keys_index++;
     }
 }
 
