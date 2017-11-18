0bba7f484228bd9f7de9b0a019b9b05a5999c1cd
joakimthun <joakimthun@gmail.com>
2017-11-18 08:09:31 +0100

    Added ADD_REMOVE_5000_RANDOM_KEYS and LOAD_5000 tests

--- tests/bp_tree_default_tests.cpp
+++ tests/bp_tree_default_tests.cpp
@@ -6,62 +6,6 @@
 
 using namespace niffler;
 
-//TEST(BP_TREE_DEFAULT, TEMP)
-//{
-//    /*for (auto i = 0; i < 50; i++)
-//    {
-//        random_keys_test(i, 50);
-//        std::cout << i << " done" << std::endl;
-//    }*/
-//
-//    //c:\dev\nifflerdb\tests\bp_tree_default_tests.cpp(64) : error:       Expected: true
-//    //To be equal to : exi
-//    //Which is : false
-//    //exists key : 14794 remove key : 10777 i : 22, seed : 29
-//
-//
-//    /*for (auto i = 0; i < 100; i++)
-//    {
-//        random_keys_test(i, 100);
-//        std::cout << i << " done" << std::endl;
-//    }*/
-//
-//    //random_keys_test(0, 200);
-//
-//    /*for (auto i = 0; i < 5; i++)
-//    {
-//        random_keys_test(i, 500);
-//        std::cout << std::endl << std::endl << i << " done ---------------- " << std::endl;
-//    }*/
-//
-//    //random_keys_test(2, 500);
-//    //random_keys_test(3, 500);
-//
-//    //random_keys_test(29, 100);
-//    //random_keys_test(30, 800);
-//
-//    //random_keys_test(77, 30);
-//    //random_keys_test(29, 100);
-//
-//    /*for (auto i = 0; i < 100; i++)
-//    {
-//        random_keys_test(117, i);
-//    }*/
-//
-//    //random_keys_test(28, 50);
-//    //random_keys_test(28, 100);
-//
-//    for (auto i = 0; i < 500; i++)
-//    {
-//        random_keys_test(i, 100);
-//        std::cout << i << " done" << std::endl;
-//    }
-//
-//    //random_keys_test(77, 100);
-//    //random_keys_test(176, 100);
-//    //random_keys_test(77, 100);
-//}
-
 TEST(BP_TREE_DEFAULT, ADD_REMOVE_1000)
 {
     auto t = bp_tree<DEFAULT_TREE_ORDER>::create(create_storage_provider("files/test_default.ndb")).value;
@@ -88,6 +32,12 @@ TEST(BP_TREE_DEFAULT, ADD_REMOVE_1000_RANDOM_KEYS)
     random_keys_test(499, 1000);
 }
 
+TEST(BP_TREE_DEFAULT, ADD_REMOVE_5000_RANDOM_KEYS)
+{
+    auto t = bp_tree<DEFAULT_TREE_ORDER>::create(create_storage_provider("files/test_default.ndb")).value;
+    random_keys_test(29, 5000);
+}
+
 TEST(BP_TREE_DEFAULT, LOAD_1000)
 {
     const auto num_keys = 1000;
@@ -105,6 +55,29 @@ TEST(BP_TREE_DEFAULT, LOAD_1000)
     
     auto loaded_t = bp_tree<DEFAULT_TREE_ORDER>::load(create_storage_provider("files/test_default.ndb", file_mode::read_update)).value;
     
+    for (auto i = 0; i < num_keys; i++)
+    {
+        EXPECT_EQ(true, loaded_t->exists(i));
+    }
+}
+
+TEST(BP_TREE_DEFAULT, LOAD_5000)
+{
+    const auto num_keys = 5000;
+
+    {
+        auto t = bp_tree<DEFAULT_TREE_ORDER>::create(create_storage_provider("files/test_default.ndb")).value;
+
+        for (auto i = 0; i < num_keys; i++)
+        {
+            EXPECT_EQ(true, t->insert(i, i));
+            auto result = validate_bp_tree(t);
+            EXPECT_EQ(true, result.valid) << result.message << std::endl << "key: " << i;
+        }
+    }
+
+    auto loaded_t = bp_tree<DEFAULT_TREE_ORDER>::load(create_storage_provider("files/test_default.ndb", file_mode::read_update)).value;
+
     for (auto i = 0; i < num_keys; i++)
     {
         EXPECT_EQ(true, loaded_t->exists(i));
